b4a2784357c20f03c3b8b6a3c3ecba2ea017c6e6
Pedro Costa <up201605339@fe.up.pt>
2018-02-22 13:49:59 +0000

    Task 3 [Completed]. Collision feature between hero and another object. Door system.

--- DungeonKeep/src/Level.java
+++ DungeonKeep/src/Level.java
@@ -1,101 +1,163 @@
+import java.util.Vector;
 
 public class Level {
-	private char map[][];
-	private Hero hero;
-	private boolean leverOn = false;
-	private boolean winningCondition = false;
-	private Pair Lever = new Pair(0,0);
+    private char map[][];
+    private Hero hero;
+    private boolean leverOn = false;
+    private boolean terminate = false;
+    private Pair Lever = new Pair(0, 0);
+    private Pair Guard = new Pair(0, 0);
+    private Vector<Pair> passageDoors = new Vector<Pair>(0);
 
 
-	private void findLever(){
+    private void findPassageDoors() {
+
         for (int i = 0; i < map.length; i++) {
-            for (int j = 0; j < map[i].length;j++)
-                if(map[i][j]== 'k')
-                {
-                    Lever.setX(i);
-                    Lever.setY(j);
+
+            if (i == 0 || i == (map.length - 1)) {
+                for (int j = 0; j < map[i].length; j++) {
+
+                    if (map[i][j] == 'I')
+                        passageDoors.add(new Pair(i, j));
                 }
+            } else {
+                if (map[i][0] == 'I') {
+                    Pair newDoor = new Pair(i, 0);
+                    passageDoors.add(newDoor);
+                }
+                if (map[i][map[i].length - 1] == 'I')
+                    passageDoors.add(new Pair(i, map[i].length - 1));
+            }
+
 
         }
     }
 
-	public Level(char level[][]) {
-		map = level;
-		hero = new Hero(map);
-		findLever();
-	}
 
-	public void printMap() {
+    private void findGameElements() {
+        for (int i = 0; i < map.length; i++) {
+            for (int j = 0; j < map[i].length; j++) {
 
+                switch (map[i][j]) {
+                    case 'k':
+                        Lever.setX(i);
+                        Lever.setY(j);
+                        break;
 
-	    for (int i = 0; i < map.length; i++){
-	        for (int j = 0; j < map[i].length; j++){
-
-	            if(hero.getX() == i && hero.getY() == j){
-	                System.out.print("H ");
+                    case 'G':
+                        Guard.setX(i);
+                        Guard.setY(j);
+                        break;
                 }
-                else
-                    System.out.print(map[i][j] + " " );
+            }
+
+        }
+    }
+
+
+    private boolean collision(Pair A){
+
+        int hero_x = hero.getX();
+        int hero_y = hero.getY();
+
+        int obj_x = A.getX();
+        int obj_y = A.getY();
+
+
+        if((Math.abs(hero_x - obj_x) + Math.abs(hero_y - obj_y)) <= 1)
+            return true;
+
+        return false;
+
+    }
+
+    public Level(char level[][]) {
+        map = level;
+        hero = new Hero(map);
+        findGameElements();
+        findPassageDoors();
+    }
+
+    public void printMap() {
+
+
+        for (int i = 0; i < map.length; i++) {
+            for (int j = 0; j < map[i].length; j++) {
+
+                if (hero.getX() == i && hero.getY() == j) {
+                    System.out.print("H ");
+                } else
+                    System.out.print(map[i][j] + " ");
 
             }
 
             System.out.println();
         }
-	}
+    }
 
-	public void updateGame() {
-		Hero.MOVEMENT_TYPE move = hero.move(map);
+    public void updateGame() {
+        Hero.MOVEMENT_TYPE move = hero.move(map);
 
-		int x = hero.getX();
-		int y = hero.getY();
+        int x = hero.getX();
+        int y = hero.getY();
 
 
-		if( x == Lever.getX() && y == Lever.getY()){
+        if (x == Lever.getX() && y == Lever.getY()) {
 
+            for (int i = 0; i < passageDoors.size(); i++) {
+
+                int a = passageDoors.elementAt(i).getX();
+                int b = passageDoors.elementAt(i).getY();
+
+                map[a][b] = 'S';
+
+
+            }
 
         }
 
-		switch (move) {
+        switch (move) {
 
-		case UP: {
-			if (map[x - 1][y] != 'X' && map[x - 1][y] != 'I') {
-				hero.setX(--x);
-			}
-			break;
-		}
-		case DOWN: {
-			if (map[x + 1][y] != 'X' && map[x + 1][y] != 'I') {
-				hero.setX(++x);
-			}
-			break;
-		}
-		case LEFT: {
-			if (map[x][y - 1] != 'X' && map[x][y - 1] != 'I') {
-				hero.setY(--y);
-			}
-			break;
-		}
-		case RIGHT: {
-			if (map[x][y + 1] != 'X' && map[x][y + 1] != 'I') {
-				hero.setY(++y);
-			}
-			break;
-		}
+            case UP: {
+                if (map[x - 1][y] != 'X' && map[x - 1][y] != 'I') {
+                    hero.setX(--x);
+                }
+                break;
+            }
+            case DOWN: {
+                if (map[x + 1][y] != 'X' && map[x + 1][y] != 'I') {
+                    hero.setX(++x);
+                }
+                break;
+            }
+            case LEFT: {
+                if (map[x][y - 1] != 'X' && map[x][y - 1] != 'I') {
+                    hero.setY(--y);
+                }
+                break;
+            }
+            case RIGHT: {
+                if (map[x][y + 1] != 'X' && map[x][y + 1] != 'I') {
+                    hero.setY(++y);
+                }
+                break;
+            }
 
-		}
-
-
+        }
 
 
+        if (map[hero.getX()][hero.getY()] == 'S' || collision(Guard))
+            terminate = true;
 
-	}
+    }
 
-	public void game() {
-		do {
-			printMap();
-			updateGame();
-		} while (!winningCondition);
+    public void game() {
+        printMap();
+        do {
+            updateGame();
+            printMap();
+        } while (!terminate);
 
-	}
+    }
 
 }
