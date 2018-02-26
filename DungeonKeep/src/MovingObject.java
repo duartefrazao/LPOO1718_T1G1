21860cc29dd9706b75c086086eb80e40f8cd2ea9
Duarte-Frazao <duartemarquesmano@gmail.com>
2018-02-26 10:55:09 +0000

    Added MovingObject file

---
+++ DungeonKeep/src/MovingObject.java
@@ -0,0 +1,56 @@
+public abstract class MovingObject {
+	
+	Pair position = new Pair(0,0);
+	
+	public enum MOVEMENT_TYPE {
+	    UP, DOWN, LEFT, RIGHT, NONE
+	}
+	
+	public void setX(int x){
+		position.setX(x);
+	}
+	public void setY(int y){
+		position.setY(y);
+	}
+	
+	public int getX(){
+		return position.getX();
+	}
+	
+	public int getY(){
+		return position.getY();
+	}	
+	public void move(MovingObject.MOVEMENT_TYPE movement, char map[][])
+	{
+		int x=position.getX();
+		int y= position.getY();
+		
+		switch (movement) {
+
+	       case UP: {
+	            if (map[x - 1][y] != 'X' && map[x - 1][y] != 'I') {
+	                position.setX(--x);
+	            }
+	            break;
+	        }
+	        case DOWN: {
+	            if (map[x + 1][y] != 'X' && map[x + 1][y] != 'I') {
+	                position.setX(++x);
+	            }
+	            break;
+	        }
+	        case LEFT: {
+	            if (map[x][y - 1] != 'X' && map[x][y - 1] != 'I') {
+	                position.setY(--y);
+	            }
+	            break;
+	        }
+	        case RIGHT: {
+	            if (map[x][y + 1] != 'X' && map[x][y + 1] != 'I') {
+	                position.setY(++y);
+	            }
+	            break;
+	        }
+	    }
+	}
+}
