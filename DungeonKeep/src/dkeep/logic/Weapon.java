330a764121dd0149d1663821fab17de65ee32e0c
Unknown <up201605339@fe.up.pt>
2018-03-06 18:22:45 +0000

    Task 2 and 3 [Completed]

--- DungeonKeep/src/dkeep/logic/Weapon.java
+++ DungeonKeep/src/dkeep/logic/Weapon.java
@@ -9,35 +9,67 @@ public class Weapon extends MovingObject {
     public char getSymbol(){
         return Symbol;
     }
-
     
-    public void move(char map[][], Pair A) {
-
-        int x = A.getX();
+    public MOVEMENT_TYPE getMove(char map[][], Pair A) {
+    	
+    	
+    	this.setX(A.getX());
+    	this.setY(A.getY());
+    	
+    	int x = A.getX();
         int y = A.getY();
 
-        Vector<Pair> possibleMovs = new Vector<Pair>(0);
+        Vector<MOVEMENT_TYPE> possibleMovs = new Vector<>();
 
 
         if (map[x - 1][y] != 'X' && map[x - 1][y] != 'I') {
-            possibleMovs.addElement(new Pair(x-1,y));
+            possibleMovs.addElement(MOVEMENT_TYPE.UP);
         }
         if (map[x + 1][y] != 'X' && map[x + 1][y] != 'I') {
-            possibleMovs.addElement(new Pair(x+1,y));
+            possibleMovs.addElement(MOVEMENT_TYPE.DOWN);
         }
         if (map[x][y - 1] != 'X' && map[x][y - 1] != 'I') {
-            possibleMovs.addElement(new Pair(x,y-1));
+            possibleMovs.addElement(MOVEMENT_TYPE.LEFT);
         }
         if (map[x][y + 1] != 'X' && map[x][y + 1] != 'I') {
-            possibleMovs.addElement(new Pair(x,y+1));
+            possibleMovs.addElement(MOVEMENT_TYPE.RIGHT);
         }
 
         int rand = ThreadLocalRandom.current().nextInt(0, possibleMovs.size());
-
-        setX(possibleMovs.elementAt(rand).getX());
-        setY(possibleMovs.elementAt(rand).getY());
-
+    	
+        
+        return possibleMovs.elementAt(rand);
     }
+
+    
+//    public void move(char map[][], Pair A) {
+//
+//        int x = A.getX();
+//        int y = A.getY();
+//
+//        Vector<Pair> possibleMovs = new Vector<Pair>(0);
+//
+//
+//        if (map[x - 1][y] != 'X' && map[x - 1][y] != 'I') {
+//            possibleMovs.addElement(new Pair(x-1,y));
+//        }
+//        if (map[x + 1][y] != 'X' && map[x + 1][y] != 'I') {
+//            possibleMovs.addElement(new Pair(x+1,y));
+//        }
+//        if (map[x][y - 1] != 'X' && map[x][y - 1] != 'I') {
+//            possibleMovs.addElement(new Pair(x,y-1));
+//        }
+//        if (map[x][y + 1] != 'X' && map[x][y + 1] != 'I') {
+//            possibleMovs.addElement(new Pair(x,y+1));
+//        }
+//
+//        int rand = ThreadLocalRandom.current().nextInt(0, possibleMovs.size());
+//        
+//
+//        setX(possibleMovs.elementAt(rand).getX());
+//        setY(possibleMovs.elementAt(rand).getY());
+//
+//    }
 
     public Weapon() {
     	super();
