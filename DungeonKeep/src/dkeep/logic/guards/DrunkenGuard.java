d9551124c7ac45a0a46bbcd79a4aca0446d768d9
Duarte-Frazao <duartemarquesmano@gmail.com>
2018-04-03 17:57:53 +0100

    Fix to drunken guard

--- DungeonKeep/src/dkeep/logic/guards/DrunkenGuard.java
+++ DungeonKeep/src/dkeep/logic/guards/DrunkenGuard.java
@@ -11,43 +11,49 @@ public class DrunkenGuard extends Guard {
 	static private boolean positiveDirection = true;
 
 	static private boolean wokeUp = false;
-	
+
 	public boolean isSleeping() {
 		return this.roundsLeftSleeping != 0;
 	}
-	
+
 	public DrunkenGuard(int x, int y) {
 		super(x, y);
 	}
 
-	public void handleNextMove(MOVEMENT_TYPE move) {
+	public void handleDirection(MOVEMENT_TYPE move) {
 		if (positiveDirection)
 			move = this.guardMovement.elementAt(currentMovPos);
 		else
 			move = contrary(this.guardMovement.elementAt(currentMovPos));
 
-		if(wokeUp) {
-            Random randDirectionChanger = new Random();
-            int directionChanger = randDirectionChanger.nextInt(4);
+		if (wokeUp) {
+			Random randDirectionChanger = new Random();
+			int directionChanger = randDirectionChanger.nextInt(4); // Value from 0 to 3 (1/4 chance of changing
+																	// direction)
 
-            if (directionChanger == 0) {
-                positiveDirection = (!positiveDirection);
+			if (directionChanger == 0) {
+				positiveDirection = (!positiveDirection);
 
-                if (positiveDirection) currentMovPos--;
-                else currentMovPos++;
-            }
+				// When changing direction it need's to change to the last/next directions
+				if (positiveDirection)
+					currentMovPos--;
+				else
+					currentMovPos++;
+			}
 
-            wokeUp= false;
-        }
+			wokeUp = false;
+		}
+	}
+
+	public void handleNextMove(MOVEMENT_TYPE move) {
 
 		Random randSleep = new Random();
-		int fallAsleep = randSleep.nextInt(7); 
+		int fallAsleep = randSleep.nextInt(7); // Value from 0 to 6 (1/6 chance of falling asleep)
 
 		if (fallAsleep == 0) {
-			roundsLeftSleeping = randSleep.nextInt(5) + 1; 
+			roundsLeftSleeping = randSleep.nextInt(5) + 1; // Value from 1 to 5 (Up to 5 rounds asleep)
 			Symbol = 'g';
 		}
-
 
 		if (positiveDirection)
 			currentMovPos++;
@@ -55,25 +61,63 @@ public class DrunkenGuard extends Guard {
 
 			currentMovPos--;
 			if (currentMovPos < 0)
-				currentMovPos = guardMovement.size() - 1;
+				currentMovPos = guardMovement.size() - 1;// Handle negative values
 		}
 
 		currentMovPos = (currentMovPos % guardMovement.size());
 
 	}
-	
-	
-	
+
+	public void wokeUpHandler() {
+		if (wokeUp) {
+			Random randDirectionChanger = new Random();
+			int directionChanger = randDirectionChanger.nextInt(4); // Value from 0 to 3 (1/4 chance of changing
+																	// direction)
+
+			if (directionChanger == 0) {
+				positiveDirection = (!positiveDirection);
+
+				// When changing direction it need's to change to the last/next directions
+				if (positiveDirection)
+					currentMovPos--;
+				else
+					currentMovPos++;
+			}
+
+			wokeUp = false;
+		}
+
+	}
+	public void moveHandler() {
+		Random randSleep = new Random();
+		int fallAsleep = randSleep.nextInt(7); // Value from 0 to 6 (1/6 chance of falling asleep)
+
+		if (fallAsleep == 0) {
+			roundsLeftSleeping = randSleep.nextInt(5) + 1; // Value from 1 to 5 (Up to 5 rounds asleep)
+			Symbol = 'g';
+		}
+
+		if (positiveDirection)
+			currentMovPos++;
+		else {
+
+			currentMovPos--;
+			if (currentMovPos < 0)
+				currentMovPos = guardMovement.size() - 1;// Handle negative values
+		}
+
+		currentMovPos = (currentMovPos % guardMovement.size());
+	}
+
 	@Override
 	public MOVEMENT_TYPE getMove() {
 
-        //Handle negative values
-        if (currentMovPos < 0)
-            currentMovPos = guardMovement.size() - 1;
+		// Handle negative values
+		if (currentMovPos < 0)
+			currentMovPos = guardMovement.size() - 1;
 
-        //Handle circular iteration through possible movements
-        currentMovPos = (currentMovPos % guardMovement.size());
-
+		// Handle circular iteration through possible movements
+		currentMovPos = (currentMovPos % guardMovement.size());
 
 		if (roundsLeftSleeping > 1) {
 			roundsLeftSleeping--;
@@ -82,14 +126,21 @@ public class DrunkenGuard extends Guard {
 			roundsLeftSleeping--;
 			this.Symbol = 'G';
 
-            wokeUp = true;
+			wokeUp = true;
 			return MovingObject.MOVEMENT_TYPE.NONE;
 		}
 
 		MovingObject.MOVEMENT_TYPE move = this.guardMovement.elementAt(currentMovPos);
-		
-		handleNextMove(move);
-		
+
+		if (positiveDirection)
+			move = this.guardMovement.elementAt(currentMovPos);
+		else
+			move = contrary(this.guardMovement.elementAt(currentMovPos));
+
+		wokeUpHandler();
+
+		moveHandler();
+
 		return move;
 
 	}
