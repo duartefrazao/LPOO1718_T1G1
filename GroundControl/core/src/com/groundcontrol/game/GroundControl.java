7c44c0b2bb7f85a5c2b27052f43720b74c39a8f7
Unknown <up201605339@fe.up.pt>
2018-04-17 17:50:43 +0100

    initial separation into mvc

--- GroundControl/core/src/com/groundcontrol/game/GroundControl.java
+++ GroundControl/core/src/com/groundcontrol/game/GroundControl.java
@@ -1,33 +1,38 @@
 package com.groundcontrol.game;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.groundcontrol.game.view.GameView;
 
-public class GroundControl extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+public class GroundControl extends Game {
+	private SpriteBatch batch;
+	private AssetManager assetManager;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		assetManager = new AssetManager();
+
+		startGame();
+	}
+
+	private void startGame(){
+		setScreen(new GameView(this));
 	}
 
 	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+		assetManager.dispose();
 	}
+
+	public SpriteBatch getBatch() {
+		return batch;
+	}
+
+	public AssetManager getAssetManager() {
+		return assetManager;
+	}
+
 }
---
+++ GroundControl/core/src/com/groundcontrol/game/view/GameView.java
@@ -0,0 +1,57 @@
+package com.groundcontrol.game.view;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.groundcontrol.game.GroundControl;
+
+public class GameView extends ScreenAdapter {
+
+    private final GroundControl game;
+
+    public final static float PIXEL_TO_METER = 0.02f;
+
+
+
+    private final OrthographicCamera camera;
+
+    public GameView(GroundControl game){
+
+        this.game = game;
+        loadAssets();
+
+        camera = createCamera();
+    }
+
+    private OrthographicCamera createCamera(){
+
+        OrthographicCamera camera = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+
+        camera.position.set(camera.viewportWidth / 2f, camera.viewportHeight / 2f, 0);
+        camera.update();
+
+        return camera;
+
+    }
+
+    private void loadAssets(){
+        this.game.getAssetManager().load("badlogic.jpg", Texture.class);
+        this.game.getAssetManager().finishLoading();
+    }
+
+    @Override
+    public void render(float delta){
+
+        Gdx.gl.glClearColor( 103/255f, 69/255f, 117/255f, 1 );
+        Gdx.gl.glClear( GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT );
+
+        game.getBatch().begin();
+        Texture bad = game.getAssetManager().get("badlogic.jpg", Texture.class);
+        game.getBatch().draw(bad, camera.position.x - (bad.getWidth() /2f), camera.position.y - (bad.getHeight() / 2f));
+        game.getBatch().end();
+
+    }
+
+}
